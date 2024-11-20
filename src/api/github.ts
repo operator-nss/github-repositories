33630bc53bd109b97e1a7de6bf498c66c4512986
Aleksey.Sokolov <PP@pXo2ME1>
2024-11-20 03:26:26 +0300

    new token

--- src/api/github.ts
+++ src/api/github.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // const GITHUB_TOKEN = import.meta.env.VITE_GITHUB_TOKEN;
-const GITHUB_TOKEN = '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'
+const GITHUB_TOKEN = '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'
 
 const githubAPI = axios.create({
   baseURL: 'https://api.github.com',
